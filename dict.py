2863f935f666dc8571303a8596bec50d25ad5718
Tanya <thetwtanya@gmail.com>
2022-04-16 17:00:59 +0800

    add remark

--- dict.py
+++ dict.py
@@ -1,11 +1,13 @@
 # dictionary 字典 = dict
+# 'ramen' 就是key
+# '拉麵' 就是value
 
 words = {
 	'ramen': '拉麵',
 	'pasta': '義大利麵'
 }
 
-words['tea'] = '茶'
+words['tea'] = '茶' # 增加新的key
 
 print(words['ramen'])
 print(words['pasta'])
